e1dc346a7b2f64ddb337a84584326ea4837c0fb3
Timo Stollenwerk <tisto@plone.org>
2018-09-14 14:30:11 +0200

    Add robot test example.

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,71 @@
 FROM jenkins/jenkins:lts
-RUN /usr/local/bin/install-plugins.sh git workflow-aggregator lockable-resources docker-build-publish parameterized-scheduler
+
+# install system packages
+USER root
+RUN apt-get update
+RUN apt-get install -y python-pip
+RUN apt-get install -y python-virtualenv
+
+# Create a default user
+RUN useradd automation --shell /bin/bash --create-home
+
+# install chrome
+# RUN curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+# RUN echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+# RUN apt-get -y update
+# RUN apt-get -y install google-chrome-stable
+
+# install chromedriver
+RUN wget "http://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip"
+RUN unzip chromedriver_linux64.zip
+RUN mv chromedriver /usr/local/bin
+
+RUN apt-get -yqq update && \
+  apt-get -yqq install curl unzip && \
+  apt-get -yqq install xvfb tinywm && \
+  apt-get -yqq install fonts-ipafont-gothic xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic && \
+  apt-get -yqq install python && \
+  rm -rf /var/lib/apt/lists/*
+
+# Install Chrome WebDriver
+# RUN CHROMEDRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` && \
+#   mkdir -p /opt/chromedriver-$CHROMEDRIVER_VERSION && \
+#   curl -sS -o /tmp/chromedriver_linux64.zip http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip && \
+#   unzip -qq /tmp/chromedriver_linux64.zip -d /opt/chromedriver-$CHROMEDRIVER_VERSION && \
+#   rm /tmp/chromedriver_linux64.zip && \
+#   chmod +x /opt/chromedriver-$CHROMEDRIVER_VERSION/chromedriver && \
+#   ln -fs /opt/chromedriver-$CHROMEDRIVER_VERSION/chromedriver /usr/local/bin/chromedriver
+
+# Install Google Chrome
+RUN curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+  echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list && \
+  apt-get -yqq update && \
+  apt-get -yqq install google-chrome-stable && \
+  rm -rf /var/lib/apt/lists/*
+
+# Disable the SUID sandbox so that Chrome can launch without being in a privileged container.
+# One unfortunate side effect is that `google-chrome --help` will no longer work.
+RUN dpkg-divert --add --rename --divert /opt/google/chrome/google-chrome.real /opt/google/chrome/google-chrome && \
+  echo "#!/bin/bash\nexec /opt/google/chrome/google-chrome.real --disable-setuid-sandbox \"\$@\"" > /opt/google/chrome/google-chrome && \
+  chmod 755 /opt/google/chrome/google-chrome
+
+# install jenkins add-ons
+USER jenkins
+RUN /usr/local/bin/install-plugins.sh git workflow-aggregator lockable-resources docker-build-publish parameterized-scheduler robot
+
+# copy files
+COPY tests/robot-tests/test1.robot  /var/jenkins_home/jobs/robot-tests/test1.robot
+COPY tests/robot-tests/test2.robot  /var/jenkins_home/jobs/robot-tests/test2.robot
+
+USER root
+RUN chown -R jenkins:jenkins  /var/jenkins_home/jobs/robot-tests
+
+# Default configuration
+ENV DISPLAY :20.0
+ENV SCREEN_GEOMETRY "1440x900x24"
+ENV CHROMEDRIVER_PORT 4444
+ENV CHROMEDRIVER_WHITELISTED_IPS "127.0.0.1"
+ENV CHROMEDRIVER_URL_BASE ''
+ENV CHROMEDRIVER_EXTRA_ARGS ''
+
+EXPOSE 4444
---
+++ tests/robot-tests/test_robot_tests.py
@@ -0,0 +1,136 @@
+import pytest
+import requests
+import time
+import os
+
+EMPTY_PIPELINE_XML = '''<?xml version='1.1' encoding='UTF-8'?>
+<flow-definition plugin="workflow-job@2.18">
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2.47">
+    <script>{}</script>
+    <sandbox>true</sandbox>
+  </definition>
+  <triggers/>
+  <disabled>false</disabled>
+</flow-definition>'''
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print(
+                '%r  %2.2f ms' %
+                (method.__name__, (te - ts) * 1000)
+            )
+        return result
+    return timed
+
+
+@timeit
+def pull_jenkins_docker_image(client):
+    # docker pull jenkins/jenkins:lts
+    jenkins_image_present = len([
+        x.tags for x in client.images.list()
+        if 'jenkins/jenkins:lts' in x.tags
+    ]) > 0
+    if not jenkins_image_present:
+        client.images.pull('jenkins/jenkins:lts')
+
+
+@timeit
+def build_jenkins_docker(client, reuse=True):
+    # docker build -t jenkins .
+    container_exists = len([
+        x.tags for x in client.images.list()
+        if 'pytest-jenkins:latest' in x.tags]
+    ) > 0
+    if container_exists and not reuse:
+        client.images.remove('pytest-jenkins:latest')
+
+    # import pdb; pdb.set_trace()
+    if not container_exists:
+        client.images.build(path='.', tag='pytest-jenkins')
+
+
+@timeit
+def run_jenkins_docker(client):
+    # docker run -p 8080:8080 -p 50000:50000 --env JAVA_OPTS="-Djenkins.install.runSetupWizard=false" jenkins &
+    container = client.containers.run(
+        'pytest-jenkins:latest',
+        environment={
+            'JAVA_OPTS': '-Djenkins.install.runSetupWizard=false'
+        },
+        # name='pytest-jenkins',
+        ports={'8080/tcp': 8080},
+        detach=True
+    )
+    print('Waiting for Jenkins to come online')
+    while True:
+        if 'setting agent port for jnlp'.encode() in container.logs():
+            break
+            # yield container
+    print('Jenkins is up and running.')
+    return container
+
+
+@pytest.fixture(scope="module")
+def jenkins():
+    import docker
+    client = docker.from_env()
+
+    # pull_jenkins_docker_image(client)
+
+    # build_jenkins_docker(client)
+    container = run_jenkins_docker(client)
+    yield container
+    print('Stop Jenkins container.')
+    container.stop()
+
+
+def test_ehlo(jenkins):
+    response = requests.get('http://localhost:8080')
+    assert response.status_code == 200
+
+
+def test_can_create_pipeline_job(jenkins):
+    import jenkins
+
+    server = jenkins.Jenkins('http://localhost:8080')
+    for job in server.get_jobs():
+        server.delete_job(job.get('name'))
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    jenkins_job_file = os.path.join(current_dir, 'Jenkinsfile')
+    jenkins_job = open(jenkins_job_file, 'r').read()
+    server.create_job('test', EMPTY_PIPELINE_XML.format(jenkins_job))
+    assert 'test' in [job.get('name') for job in server.get_jobs()]
+
+
+def test_can_run_pipeline_job(jenkins):
+    import jenkins
+
+    server = jenkins.Jenkins('http://localhost:8080')
+    for job in server.get_jobs():
+        server.delete_job(job.get('name'))
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    jenkins_job_file = os.path.join(current_dir, 'Jenkinsfile')
+    jenkins_job = open(jenkins_job_file, 'r').read()
+    server.create_job('test', EMPTY_PIPELINE_XML.format(jenkins_job))
+    server.build_job('test')
+    # wait for build to complete
+    while server.get_job_info('test').get('lastCompletedBuild') is None:
+        print('.')
+        time.sleep(1)
+    jenkins_build_result = server.get_build_info('test', 1).get('result')
+    if jenkins_build_result != 'SUCCESS':
+        print(server.get_build_console_output('test', 1))
+    assert 'SUCCESS' == jenkins_build_result
